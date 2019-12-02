947fa809374497434a73ce138a3828aeba38603d
marianalfr <marianalfr@gmail.com>
2019-12-02 17:36:35 +0100

    Save favs to LS and show them within list (some issues yet to fix)

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,6 +5,7 @@ const buttonSearch = document.querySelector('#btn-search');
 const showList = document.querySelector('#show-list');
 const formSearch = document.querySelector('#form-search');
 let favourites = [];
+const favList = document.querySelector('#fav-list');
 
 function getShowsFromAPI(){
     const inputSearchValue = inputSearch.value;
@@ -58,6 +59,7 @@ function paintShows(allShows){
         elementUl.appendChild(elementLi);
 
         elementLi.addEventListener('click', selectShow);
+        elementLi.addEventListener('click', saveShow);
      }
 }
 
@@ -81,7 +83,66 @@ function clearSearch(){
 }
 
 function selectShow(event){
-    event.currentTarget.classList.toggle('selected')
+    event.currentTarget.classList.toggle('selected');
+}
+
+function saveShow(event){
+    const currentShowTitle = event.currentTarget.querySelector('.select-item');
+    const currentShowImage = event.currentTarget.querySelector('.show-image');
+
+    const showObject = {
+        name: currentShowTitle.innerHTML,
+        id: currentShowTitle.getAttribute('id'),
+        image: currentShowImage.src,
+    }
+
+    if(checkLocalStorage(showObject) === false){
+        favourites = JSON.parse(localStorage.getItem('favourites'));
+        favourites.push(showObject);
+        localStorage.setItem('favourites', JSON.stringify(favourites));
+        console.log('false');
+        paintFavourites(showObject);
+    } else if (checkLocalStorage(showObject) === true){
+        favourites = JSON.parse(localStorage.getItem('favourites'));
+        localStorage.setItem('favourites', JSON.stringify(favourites));
+        console.log('true');
+    } else {
+        favourites = [];
+        favourites.push(showObject);
+        localStorage.setItem('favourites', JSON.stringify(favourites));
+        console.log('else');
+        paintFavourites(showObject);
+    }
+}
+
+function checkLocalStorage(object){
+    favourites = JSON.parse(localStorage.getItem('favourites'));
+    if (favourites !==null){
+        const mySavedFavourites = JSON.parse(localStorage.getItem('favourites'));
+
+        for (let savedFavourite of mySavedFavourites){
+            if (savedFavourite.id === object.id){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+
+function paintFavourites(object){
+    const favLiElement = document.createElement('li');
+    favLiElement.classList.add('fav-list-element');
+
+    const favSpanItem = document.createElement('span');
+    const favTitleItem = document.createTextNode(object.name);
+    const favImgItem = document.createElement('img');
+    favImgItem.src = object.image;
+
+    favSpanItem.appendChild(favTitleItem);
+    favLiElement.appendChild(favSpanItem);
+    favLiElement.appendChild(favImgItem);
+    favList.appendChild(favLiElement);
 }
 
 buttonSearch.addEventListener('click', getShowsFromAPI);
