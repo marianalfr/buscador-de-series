6dc5c3595ad03bc7ea3d8f531d56e8d6d3906abb
marianalfr <marianalfr@gmail.com>
2019-12-02 16:14:13 +0100

    Paint shows and intro search

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,74 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputSearch = document.querySelector('#input-search');
+const buttonSearch = document.querySelector('#btn-search');
+const showList = document.querySelector('#show-list');
+const formSearch = document.querySelector('#form-search');
+let favourites = [];
+
+function getShowsFromAPI(){
+    const inputSearchValue = inputSearch.value;
+    fetch (`http://api.tvmaze.com/search/shows?q=${inputSearchValue}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data.length !== 0){
+            paintShows(data)
+        } else {
+            paintError();
+        }
+    })
+}
+
+function paintShows(allShows){
+
+    const defaultImage = 'https://via.placeholder.com/210x295/ffffff/666666/?%20text=TV';
+
+    const elementUl = document.createElement('ul');
+    elementUl.classList.add('search-list');
+    showList.appendChild(elementUl);
+
+    for (let show of allShows){
+        const showData = show.show;
+        const elementLi = document.createElement('li');
+        elementLi.classList.add('search-item');
+
+        const showID = showData.id;
+        const showName = showData.name;
+
+        const elementShowTitle = document.createElement('h2');
+        elementShowTitle.setAttribute('id', showID);
+        elementShowTitle.classList.add('select-item');
+        const elementShowImage = document.createElement('img');
+        elementShowImage.classList.add('show-image');
+        const showTitle = document.createTextNode (showName);
+        
+
+        elementShowTitle.appendChild(showTitle);
+
+        if (showData.image !== null){
+            elementShowImage.src = showData.image.medium;
+        } else {
+            elementShowImage.src = defaultImage;
+        }
+        
+        elementLi.appendChild(elementShowTitle);
+        elementLi.appendChild(elementShowImage);
+        elementUl.appendChild(elementLi);
+     }
+}
+
+function paintError(){
+    const errorText = document.createElement('p');
+    errorText.classList.add('error-message');
+    const errorMessage = document.createTextNode('Lo siento mucho, no conozco esta esta serie.');
+    errorText.appendChild(errorMessage);
+    showList.appendChild(errorText);
+}
+
+function introSearch(){
+    event.preventDefault();
+    getShowsFromAPI();
+}
+
+buttonSearch.addEventListener('click', getShowsFromAPI);
+formSearch.addEventListener('submit', introSearch);
